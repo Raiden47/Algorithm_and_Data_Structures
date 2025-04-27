c1c3c2746fc451d6b7913963a946cbb3f1e77ca3
LUIGI TAGLIAFERRI <l.tagliaferri@studenti.unina.it>
2025-04-27 21:47:59 +0200

    n_queen problem - updated version <wip>

--- N_Queen/main_v2.py
+++ N_Queen/main_v2.py
@@ -1,7 +1,6 @@
-def n_queen(mat) :
+def n_queen(mat, row, col) :
     result = []
     
-    row = col = 0
     while True :
         row, col = find_pos(mat, row, col)
         if row is None or col is None :
@@ -11,6 +10,18 @@ def n_queen(mat) :
         mat_adj(mat, row, col)
         
     return result
+
+def n_queen_backtrack(mat, row, col, res) :
+    new_res = n_queen(mat, row, col)
+    print(res)
+    print (new_res)
+    if col >= len(mat) :
+        return res
+    
+    if len(res) < len(new_res) :
+        return n_queen_backtrack(mat, row, col + 1, new_res)
+    
+    return n_queen_backtrack(mat, row, col + 1, res)
 
 def find_pos(mat, row, col) :
     if row >= len(mat) : 
@@ -34,8 +45,9 @@ def mat_adj(mat, row, col):
     
 
 if __name__ == '__main__' :
+    result = []
     dim = int(input("<--- Insert board dimendion --->\n> "))
     mat = [[0 for _ in range(dim)] for _ in range(dim)]
-    result = n_queen(mat)
+    result = n_queen_backtrack(mat, 0, 1, result)
     print(f"Numero di queen posizionate: {len(result)}\nPosizioni delle N queen: {result}")
     
