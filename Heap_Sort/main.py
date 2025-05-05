6c61668fad56a2bfcd10e36d12708799fc9d98e4
Raiden47 <tacotag@libero.it>
2025-05-05 12:16:06 +0200

    heapsort fixes - quicksort

--- Heap_Sort/main.py
+++ Heap_Sort/main.py
@@ -2,62 +2,53 @@
 
 import random as rnd
 
-def init_arr () :
+def init_arr():
     return [rnd.randint(1, 20) for _ in range(20)]
 
 # Max_Heap -> A[PARENT(i)] >= A[i]
-class max_heap :
-    
-    lenght = 0
-    heap_size = 0
-    
-    def __init__(self, arr) :
-        self.lenght = len(arr)
-        self.heap_size = self.lenght
+class MaxHeap:
+    def __init__(self, arr):
+        self.arr = arr
+        self.heap_size = len(arr)
 
-    def parent (i) :
-        return i // 2
+    def parent(self, i):
+        return (i - 1) // 2
 
-    def left (i) :
-        return 2 * i 
+    def left(self, i):
+        return 2 * i + 1
 
-    def right (i) :
-        return (2 * i) + 1
+    def right(self, i):
+        return 2 * i + 2
 
-    def max_heapify(self, arr, i) :
-        max = 0
+    def max_heapify(self, i):
         l = self.left(i)
         r = self.right(i)
-        max = l if l <= self.heap_size and arr[l] > arr[i] else i
-        # if l <= self.heap_size and arr[l] > arr[i] :
-        if r < self.heap_size and arr[r] > arr[max] :
-            max = r
-        if max != i :
-            t = arr[i]
-            arr[i] = arr[max]
-            arr[max] = t
-            self.max_heapify(self, arr, max)
-            
-def build_max_heap (arr) :
-    heap = max_heap()
-    heap.__init__(arr)
-    for i in range(heap.lenght//2) :
-        heap.max_heapify(arr, (heap.lenght//2) - i)
-            
-def heapsort(arr) :
-    heap = max_heap()
-    build_max_heap(arr)
-    for i in range(len(arr)) :
-        if i < 1 :
-            arr[0] , a[len(arr) - 1] = a[len(arr) - 1], arr[0]
-            heap.heap_size -= 1
-            heap.max_heapify(arr, 0)
-            
-        
-    
+        largest = i
 
-if __name__ == "__main__" :
+        if l < self.heap_size and self.arr[l] > self.arr[largest]:
+            largest = l
+        if r < self.heap_size and self.arr[r] > self.arr[largest]:
+            largest = r
+
+        if largest != i:
+            self.arr[i], self.arr[largest] = self.arr[largest], self.arr[i]
+            self.max_heapify(largest)
+
+def build_max_heap(arr):
+    heap = MaxHeap(arr)
+    for i in range(len(arr) // 2 - 1, -1, -1):
+        heap.max_heapify(i)
+    return heap
+
+def heapsort(arr):
+    heap = build_max_heap(arr)
+    for i in range(len(arr) - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]  # Scambia il massimo con l'ultimo elemento
+        heap.heap_size -= 1  # Riduci la dimensione dell'heap
+        heap.max_heapify(0)  # Ripristina la proprietà di max-heap
+
+if __name__ == "__main__":
     arr = init_arr()
-    print(arr)
+    print("Array iniziale:", arr)
     heapsort(arr)
-    print(arr)
+    print("Array ordinato:", arr)
---
+++ Quick_Sort/main.py
@@ -0,0 +1,32 @@
+# Quicksort
+
+import random as rnd
+
+def init_arr () :
+    return [rnd.randint(1, 20) for _ in range(20)]
+
+def quicksort(arr, p, r) :
+    if p < r :
+        q = partition (arr, p, r)
+        quicksort(arr, p, q - 1)
+        quicksort(arr, q + 1, r)
+    
+# p punta alla prima posizione dell'array
+# r punta all'ultima posizione dell'array
+def partition (arr, p, r) :
+    x = arr[r]
+    i = p - 1
+    for j in range(p, r) :
+        if arr[j] <= x :
+            i += 1
+            arr[i] , arr[j] = arr[j], arr[i]
+    arr[i+1], arr[r] = arr[r], arr[i+1]
+    return i + 1
+        
+        
+
+if __name__ == "__main__" :
+    arr = init_arr()
+    print(arr)
+    quicksort(arr, 0, len(arr) -1)
+    print(arr)
