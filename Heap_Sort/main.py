02489322943428a3b36f1d66ed84f7481d35d7d0
Raiden47 <tacotag@libero.it>
2025-05-05 11:39:04 +0200

    heapsort wip

--- Heap_Sort/main.py
+++ Heap_Sort/main.py
@@ -7,6 +7,13 @@ def init_arr () :
 
 # Max_Heap -> A[PARENT(i)] >= A[i]
 class max_heap :
+    
+    lenght = 0
+    heap_size = 0
+    
+    def __init__(self, arr) :
+        self.lenght = len(arr)
+        self.heap_size = self.lenght
 
     def parent (i) :
         return i // 2
@@ -17,11 +24,40 @@ class max_heap :
     def right (i) :
         return (2 * i) + 1
 
-    def max_heapify(arr, i) :
+    def max_heapify(self, arr, i) :
+        max = 0
+        l = self.left(i)
+        r = self.right(i)
+        max = l if l <= self.heap_size and arr[l] > arr[i] else i
+        # if l <= self.heap_size and arr[l] > arr[i] :
+        if r < self.heap_size and arr[r] > arr[max] :
+            max = r
+        if max != i :
+            t = arr[i]
+            arr[i] = arr[max]
+            arr[max] = t
+            self.max_heapify(self, arr, max)
+            
+def build_max_heap (arr) :
+    heap = max_heap()
+    heap.__init__(arr)
+    for i in range(heap.lenght//2) :
+        heap.max_heapify(arr, (heap.lenght//2) - i)
+            
+def heapsort(arr) :
+    heap = max_heap()
+    build_max_heap(arr)
+    for i in range(len(arr)) :
+        if i < 1 :
+            arr[0] , a[len(arr) - 1] = a[len(arr) - 1], arr[0]
+            heap.heap_size -= 1
+            heap.max_heapify(arr, 0)
+            
+        
     
 
 if __name__ == "__main__" :
     arr = init_arr()
     print(arr)
-    # arr = ___ (arr)
+    heapsort(arr)
     print(arr)
