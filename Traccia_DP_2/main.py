6993306c4a0b6a8aa1f3d1ca844856149a25ae63
LUIGI TAGLIAFERRI <l.tagliaferri@studenti.unina.it>
2025-05-11 22:58:21 +0200

    .

--- Traccia_DP_2/main.py
+++ Traccia_DP_2/main.py
@@ -26,9 +26,6 @@ def cut_min (l, cuts) :
                 
     return dp[0][len(cuts)-1]
     
-        
-    
-    return 
 
 if __name__ == '__main__' :
     l, arr = init_func(1)
@@ -43,54 +40,3 @@ if __name__ == '__main__' :
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-# def init_func(n):
-#     if n == 1:
-#         return 30, [2, 20, 25]
-#     if n == 2:
-#         return 10, [4, 5, 7, 8]
-#     if n == 3:
-#         return 100, [25, 50, 75]
-#     return None, None
-
-# def cut_min(l, cuts):
-#     # Aggiungi gli estremi della trave ai tagli
-#     cuts = [0] + sorted(cuts) + [l]
-#     print(cuts)
-#     n = len(cuts)
-#     # dp[i][j] = costo minimo per tagliare tra cuts[i] e cuts[j]
-#     dp = [[0] * n for _ in range(n)]
-#     for length in range(2, n):  # lunghezza del segmento considerato
-#         for i in range(n - length):
-#             j = i + length
-#             dp[i][j] = float('inf')
-#             for k in range(i + 1, j):
-#                 cost = cuts[j] - cuts[i] + dp[i][k] + dp[k][j]
-#                 if cost < dp[i][j]:
-#                     dp[i][j] = cost
-                    
-#     print (dp)
-#     return dp[0][n - 1]
-
-# if __name__ == '__main__':
-#     l, arr = init_func(3)
-#     if l is None or arr is None:
-#         print("<--- init error --->")
-#     else:
-#         print(f"Min cost : {cut_min(l, arr)}")
